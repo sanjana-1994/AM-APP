afdbbea6bbb4d2732099236417dff0b6497d6407
sanjana-1994 <sdas199421@gmail.com>
2018-09-18 06:39:25 +0000

    session testcase added

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,16 +4,16 @@ class AuthController < ApplicationController
 
   def create
     patient = Patient.find_by_name(params[:auth][:name])
-	if patient && patient.authenticate(params[:auth][:password])
-	  session[:patient_id] = patient.id
+	  if patient && patient.authenticate(params[:auth][:password])
+	    session[:patient_id] = patient.id
       redirect_to root_path
-	else
-      redirect_to '/login'
-	end 
+	  else
+      redirect_to login_path
+	  end 
   end
 
   def destroy 
-	session[:patient_id] = nil 
-	redirect_to '/login' 
+	  session[:patient_id] = nil 
+	  redirect_to login_path 
   end
 end
--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -2,8 +2,26 @@ require 'rails_helper'
 
 RSpec.describe AuthController, 'auth#create' do
   it "if user is present, it should redirect the page to root path" do
-    user = Patient.create(name: 'ABCD', password_digest:'XYZ', role: 'admin')
-    post :create,  {auth: { name: 'ABCD', password: 'XYZ' }}
+    puts "Testing authentication of valid user "
+    user = Patient.create(name: 'ABCD', password:'XYZ', role: 'admin')
+    user.save
+    post :create, :params =>{:auth =>{:name => "ABCD", :password => "XYZ"}}
     expect(response).to redirect_to(root_path)
+  end
+
+  it "if user is not present, it should redirect the page to login path" do
+    puts "Testing authentication of invalid user "
+    user = Patient.create(name: 'ABCDE', password:'XYZ', role: 'admin')
+    user.save
+    post :create, :params =>{:auth =>{:name => "ABCD", :password => "XYZ"}}
+    expect(response).to redirect_to(login_path)
+  end
+
+  it "if user is blank, it should redirect the page to login path" do
+    puts "Testing authentication of invalid blank user "
+    user = Patient.create(name: 'ABCDE', password:'XYZ', role: 'admin')
+    user.save
+    post :create, :params =>{:auth =>{:name => "", :password => ""}}
+    expect(response).to redirect_to(login_path)
   end
 end
